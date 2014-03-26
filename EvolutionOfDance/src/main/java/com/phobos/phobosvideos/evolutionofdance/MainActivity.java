97b0ee9f0efdf131d7f8677413cf2a5f8f37ebb3
apcsoftwares <apcsoftwares@gmail.com>
2014-03-26 01:37:58 -0400

    Primer commit

--- EvolutionOfDance/src/main/java/com/phobos/phobosvideos/evolutionofdance/MainActivity.java
+++ EvolutionOfDance/src/main/java/com/phobos/phobosvideos/evolutionofdance/MainActivity.java
@@ -1,10 +1,6 @@
 package com.phobos.phobosvideos.evolutionofdance;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.phobos.phobosvideos.phobosvideoslibrary.VideoPlayback;
 
@@ -17,34 +13,4 @@ public class MainActivity extends VideoPlayback
 		super.onCreate(savedInstanceState);
 		setVideoID(R.raw.evolutionofdance);
 	}
-
-	/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-		//super.onCreate(savedInstanceState);        //setContentView(R.layout.activity_main);
-		initialize("evolutionofdance");
-
-    }*/
-
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-*/
 }
--- PhobosVideosLibrary/src/main/java/com/phobos/phobosvideos/phobosvideoslibrary/VideoPlayback.java
+++ PhobosVideosLibrary/src/main/java/com/phobos/phobosvideos/phobosvideoslibrary/VideoPlayback.java
@@ -1,10 +1,10 @@
 package com.phobos.phobosvideos.phobosvideoslibrary;
 
-import android.graphics.PixelFormat;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.MediaController;
@@ -16,12 +16,15 @@ public class VideoPlayback extends ActionBarActivity {
 	private VideoView mVideoView = null;
 	//private String videoName = null;
 
+	private String appPackageName; // getPackageName() from Context or Activity object
+
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_playback);
 		//initialize("");
 		// Get a reference to the VideoView
+		appPackageName = getPackageName();
 
 		//getWindow().setFormat(PixelFormat.TRANSLUCENT);
 		mVideoView = (VideoView) findViewById(R.id.videoViewer);
@@ -68,8 +71,9 @@ public class VideoPlayback extends ActionBarActivity {
 		super.onPause();
 	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.video_playback, menu);
@@ -82,10 +86,28 @@ public class VideoPlayback extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		if (id == R.id.share)
+		{
+			return true;
+		}
+		else if (id == R.id.more_apps)
+		{
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://goo.gl/ZCxoI")));
+			return true;
+		}
+		else if (id == R.id.rate_app)
+		{
+			try
+			{
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+			}
+			catch (android.content.ActivityNotFoundException anfe)
+			{
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+			}
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
